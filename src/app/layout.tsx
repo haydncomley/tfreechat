26100990594296e244e4853bce0ec9f63638ae11
haydncomley <haydncomley@gmail.com>
2025-06-11 20:54:07 +0100

    login init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Providers } from "~/components/providers";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,12 +24,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+'use client';
+
+import { useAuth } from "~/hooks/use-auth";
 
 export default function Home() {
+  const { user, signIn, signOut } = useAuth();
+
   return (
     <div className="w-full h-full flex items-center justify-center">
       <main>
-       Hello world
+        {user ? <p>Hello {user.displayName}</p> : <p>No user</p>}
+
+        {user ? (
+          <button className="p-2 border rounded-lg" onClick={() => signOut()}>Sign Out</button>
+        ) : (
+          <button className="p-2 border rounded-lg" onClick={() => signIn()}>Sign In</button>
+        )}
       </main>
     </div>
   );
