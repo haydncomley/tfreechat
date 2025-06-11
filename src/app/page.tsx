a88d6970f9e7f7577f2e16d78a2493b7887aa71f
haydncomley <haydncomley@gmail.com>
2025-06-11 19:56:48 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <main>
+       Hello world
+      </main>
+    </div>
+  );
+}
